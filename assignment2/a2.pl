edf8d90b9db8b9e8051fafe85a81d4507c43daa9
Trash-J <stevenlu176@gmail.com>
2023-02-27 17:30:30 +0100

    added comments

--- assignment2/a2.pl
+++ assignment2/a2.pl
@@ -1,17 +1,20 @@
-%1.1
-item(coke).
-item(hagelslag).
-grabbed(robot, item(hagelslag)).
-at_location(item(coke), table).
+%--------------------------2.1--------------------------- 
+%Facts
+item(coke). % coke is an item
+item(hagelslag). % haglesla is an item
+grabbed(robot, item(hagelslag)). % the robot grabbed the item hagelslag
+at_location(item(coke), table). % the item coke in on the table
 
-is_heavy(item(hagelslag)).
-has_room(bin).
-has_room(shelf).
+%Rules
+is_full(item(X)) :- is_heavy(item(X)). % all heavy items are full
+should_be_stored_in_shelf(item(X)) :- is_full(item(X)). % all full items should be stored in the shelf
+should_be_discarded_in_bin(item(X)) :- is_empty(item(X)). % all empty items should be discarded in the bin
+can_be_stored(item(X)) :- has_room(shelf), grabbed(robot, item(X)). % if the shelf has room and the robot has grabbed an item, the item can be discarded
+can_be_discarded(item(X)) :- has_room(bin), grabbed(robot, item(X)). % if the bin has room and the robot has grabbed an item, the item can be discarded
 
-is_full(item(X)) :- is_heavy(item(X)).
-is_empty(item(coke)).
-should_be_stored_in_shelf(item(X)) :- is_full(item(X)).
-should_be_discarded_in_bin(item(X)) :- is_empty(item(X)).
-
-can_be_stored(item(X)) :- has_room(shelf), grabbed(robot, item(X)).
-can_be_discarded(item(X)) :- has_room(bin), grabbed(robot, item(X)).
+%--------------------------2.2---------------------------
+%Facts
+is_heavy(item(hagelslag)). % the item hagelslag is heavy
+is_empty(item(coke)). % the item coke is empty
+has_room(shelf). % the shelf has room
+has_room(bin). % the bin has room
