8f067d0d146778201571847f381edf2df1837286
Trash-J <stevenlu176@gmail.com>
2023-02-27 12:35:59 +0100

    ass2

---
+++ assignment2/a2.pl
@@ -0,0 +1,16 @@
+item(coke).
+item(hagelslag).
+grabbed(robot, hagelslag).
+at_location(coke, table).
+
+is_heavy(coke).
+has_room(bin).
+has_room(shelf).
+
+is_full(X) :- is_heavy(item(X)).
+is_empty(X) :- \+ is_heavy(item(X)).
+should_be_stored_in_shelf(X) :- is_full(X).
+should_be_discarded_in_bin(X) :- is_empty(X).
+
+can_be_stored(X) :- has_room(shelf), grabbed(robot, item(X)).
+can_be_discarded(X) :- has_room(bin), grabbed(robot, item(X)).
