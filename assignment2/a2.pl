cdb2a1c16f11ba1b8a286edef8fcf30e6979eeab
Trash-J <stevenlu176@gmail.com>
2023-02-27 17:11:00 +0100

    Finalized

--- assignment2/a2.pl
+++ assignment2/a2.pl
@@ -2,16 +2,16 @@
 item(coke).
 item(hagelslag).
 grabbed(robot, item(hagelslag)).
-at_location(coke, table).
+at_location(item(coke), table).
 
 is_heavy(item(hagelslag)).
 has_room(bin).
 has_room(shelf).
 
-is_full(X) :- is_heavy(item(X)).
+is_full(item(X)) :- is_heavy(item(X)).
 is_empty(item(coke)).
-should_be_stored_in_shelf(X) :- is_full(X).
-should_be_discarded_in_bin(X) :- is_empty(X).
+should_be_stored_in_shelf(item(X)) :- is_full(item(X)).
+should_be_discarded_in_bin(item(X)) :- is_empty(item(X)).
 
-can_be_stored(X) :- has_room(shelf), grabbed(robot, item(X)).
-can_be_discarded(X) :- has_room(bin), grabbed(robot, item(X)).
+can_be_stored(item(X)) :- has_room(shelf), grabbed(robot, item(X)).
+can_be_discarded(item(X)) :- has_room(bin), grabbed(robot, item(X)).
